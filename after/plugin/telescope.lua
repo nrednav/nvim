5c95e5f040739806f06a61741773d8332af14c8b
nrednav <nrednav@gmail.com>
2023-03-18 23:28:31 +0000

    feat(telescope): install fzf as native sorter

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,4 @@
--- Mappings --
+-- Mappings
 local map = vim.keymap.set
 local builtin = require("telescope.builtin")
 map("n", "<leader>ff", builtin.find_files, {})
@@ -13,53 +13,17 @@ map("n", "<leader>vc", function()
   })
 end)
 
---[[
-
--- Setup --
-require('telescope').setup{
-  defaults = {
-    vimgrep_arguments = {
-      'rg',
-      '--color=never',
-      '--no-heading',
-      '--with-filename',
-      '--line-number',
-      '--column',
-      '--smart-case'
-    },
-    prompt_prefix = "> ",
-    selection_caret = "> ",
-    entry_prefix = "  ",
-    initial_mode = "insert",
-    selection_strategy = "reset",
-    sorting_strategy = "descending",
-    layout_strategy = "horizontal",
-    file_sorter =  require('telescope.sorters').get_fzy_sorter,
-    file_ignore_patterns = {},
-    generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
-		path_display = {
-			"shorten",
-			"absolute"
-		},
-    winblend = 0,
-    border = {},
-    borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
-    color_devicons = true,
-    use_less = true,
-    set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
-    file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
-    grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
-    qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
-
-    -- Developer configurations: Not meant for general override
-    buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
-  },
-	extensions = {
-		fzy_native = {
-			override_generic_sorter = false,
-			override_file_sorter = true,
-	  }
-	}
+-- Setup
+require('telescope').setup {
+  extensions = {
+    fzf = {
+      fuzzy = true,
+      override_generic_sorter = true,
+      override_file_sorter = true,
+      case_mode = "smart_case",
+    }
+  }
 }
 
---]]
+-- Extensions
+require("telescope").load_extension("fzf")
--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -23,6 +23,10 @@ return require('packer').startup(function(use)
     'nvim-telescope/telescope.nvim', tag = '0.1.1',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
+  use {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+  }
   use 'Shougo/defx.nvim'
   use 'theprimeagen/harpoon'
 
