97fb1d12e6202b9fb7ea26577429dd5a53d7c149
nrednav <nrednav@gmail.com>
2023-03-19 00:39:41 +0000

    feat: add cool mappings from primeagens 0-to-lsp vid

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -13,3 +13,15 @@ map("n", "<C-right>", ":tabnext<cr>", { noremap = true })
 -- Better identing
 map("v", "<", "<gv")
 map("v", ">", ">gv")
+
+-- Move highlighted blocks
+map("v", "J", ":m '>+1<cr>gv=gv")
+map("v", "K", ":m '<-2<cr>gv=gv")
+
+-- Keep cursor centered while half-page jumping
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+
+-- Keep cursor centered while navigating through search terms
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
