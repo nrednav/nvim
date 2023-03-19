ab5ea5a2a8bd10879de7e489f3433e73f8806fa4
nrednav <nrednav@gmail.com>
2023-03-19 00:39:21 +0000

    feat: add & setup lsp

--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -30,6 +30,30 @@ return require('packer').startup(function(use)
   use 'Shougo/defx.nvim'
   use 'theprimeagen/harpoon'
 
+  -- LSP
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v1.x',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},             -- Required
+      {'williamboman/mason.nvim'},           -- Optional
+      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},         -- Required
+      {'hrsh7th/cmp-nvim-lsp'},     -- Required
+      {'hrsh7th/cmp-buffer'},       -- Optional
+      {'hrsh7th/cmp-path'},         -- Optional
+      {'saadparwaiz1/cmp_luasnip'}, -- Optional
+      {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},             -- Required
+      {'rafamadriz/friendly-snippets'}, -- Optional
+    }
+  }
+
   -- Dev
   use {
     "folke/trouble.nvim",
