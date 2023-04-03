46565bfc419ddbba073b144577e6c3dcc13c42e1
nrednav <nrednav@gmail.com>
2023-04-03 03:09:11 +0100

    fix: os detection logic in globals.lua

--- lua/core/globals.lua
+++ lua/core/globals.lua
@@ -1,8 +1,10 @@
+local os = vim.loop.os_uname().sysname;
+
 -- Set leader key
 vim.g.mapleader = ","
 
 -- Set path to python binary
-if vim.fn.has("macunix") then
+if os == "Darwin" then
   vim.g.python3_host_prog='/opt/homebrew/bin/python3'
 else
   vim.g.python3_host_prog='/usr/bin/python'
