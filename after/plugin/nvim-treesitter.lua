c3801c2d5428071867222fd69ed807f53f8bc368
vandern rodrigues <vandern.rodrigues@boats.com>
2023-07-24 14:43:58 +0100

    feat: add vimdoc as parser to replace previous 'help' parser

--- after/plugin/nvim-treesitter.lua
+++ after/plugin/nvim-treesitter.lua
@@ -1,6 +1,6 @@
 require 'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "javascript", "typescript", "cpp", "go", "rust", "python", "lua", "tsx", "vim", "query" },
+  ensure_installed = { "javascript", "typescript", "cpp", "go", "rust", "python", "lua", "tsx", "vim", "vimdoc", "query" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
