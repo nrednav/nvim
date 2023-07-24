9f9301fdd9ff598993ec643543aacc295c10c835
vandern rodrigues <vandern.rodrigues@boats.com>
2023-07-24 17:54:18 +0100

    feat: add code to auto-configure packer on fresh install + a startuptime plugin

--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -1,3 +1,16 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
@@ -70,4 +83,11 @@ return require('packer').startup(function(use)
   use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
   use 'jose-elias-alvarez/null-ls.nvim'
 
+  -- Performance
+  use('dstein64/vim-startuptime')
+
+  -- Auto-Configure on fresh install
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
