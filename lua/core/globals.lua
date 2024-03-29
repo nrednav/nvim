0487325ca0e98bd537d6b0d496068de92c7e0b85
vandern rodrigues <nrednav@gmail.com>
2024-03-29 19:36:31 +0400

    refactor: update python provider path

--- lua/core/globals.lua
+++ lua/core/globals.lua
@@ -5,7 +5,7 @@ vim.g.mapleader = ","
 
 -- Set path to python binary
 if os == "Darwin" then
-  vim.g.python3_host_prog='/opt/homebrew/bin/python3'
+  vim.g.python3_host_prog='/opt/homebrew/bin/python3.11'
 else
   vim.g.python3_host_prog='/usr/bin/python'
 end
