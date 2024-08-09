d796f39ac54dc4629e41563bd9c17d8c4c12716c
Vandern Rodrigues <nrednav@gmail.com>
2024-08-09 19:08:40 +0100

    add mapping to convert words to snake_case

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -25,3 +25,22 @@ map("n", "N", "Nzzzv")
 
 -- Change to directory of currently open file
 map("n", "<leader>cd", ":cd %:p:h<CR>:pwd<CR>", { noremap = true })
+
+-- Convert camelCase to snake_case
+local function convert_to_snake_case()
+	table.unpack = table.unpack or unpack
+	local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+	local word = vim.fn.expand("<cword>")
+	local word_start = vim.fn.matchstrpos(vim.fn.getline("."), "\\k*\\%" .. (col + 1) .. "c\\k*")[2]
+
+	-- Check if the word is in camelCase
+	if word:find("[a-z][A-Z]") then
+		-- Convert camelCase to snake_case
+		local snake_case_word = word:gsub("([a-z])([A-Z])", "%1_%2"):lower()
+		vim.api.nvim_buf_set_text(0, line - 1, word_start, line - 1, word_start + #word, { snake_case_word })
+	else
+		print("Not a camelCase word")
+	end
+end
+
+map("n", "<leader>cs", convert_to_snake_case)
