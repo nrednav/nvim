e30766a232f4f5818967601d70099cf8891e4ba8
Vandern Rodrigues <nrednav@gmail.com>
2025-04-22 12:49:21 +0100

    remove <CR> as completion keybinding and add binding for a custom datetime header

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -25,3 +25,40 @@ map("n", "N", "Nzzzv")
 
 -- Change to directory of currently open file
 map("n", "<leader>cd", ":cd %:p:h<CR>:pwd<CR>", { noremap = true })
+
+-- Insert DateTime header
+local function get_day_with_ordinal(day)
+	if day > 3 and day < 21 then
+		return day .. "th"
+	end
+
+	local last_digit = day % 10
+
+	if last_digit == 1 then
+		return day .. "st"
+	elseif last_digit == 2 then
+		return day .. "nd"
+	elseif last_digit == 3 then
+		return day .. "rd"
+	else
+		return day .. "th"
+	end
+end
+
+local function get_formatted_datetime()
+	local now = os.date("*t")
+	local year = now.year
+	local month = os.date("%B", os.time(now))
+	local day = now.day
+	local day_with_ordinal = get_day_with_ordinal(day)
+	local time_24h = string.format("%02d:%02d", now.hour, now.min)
+
+	return string.format("%d - %s %s, %s", year, month, day_with_ordinal, time_24h)
+end
+
+vim.api.nvim_create_user_command("InsertDateTimeHeader", function()
+	local datetime_string = get_formatted_datetime()
+	vim.api.nvim_buf_set_lines(0, 0, 0, true, { datetime_string, "" })
+end, {})
+
+map("n", "<leader>dth", ":InsertDateTimeHeader<CR>", { noremap = true, silent = true })
--- lua/plugins/custom/completion.lua
+++ lua/plugins/custom/completion.lua
@@ -31,10 +31,6 @@ cmp.setup({
 	mapping = {
 		["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
 		["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-		["<CR>"] = cmp.mapping(
-			cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true }),
-			{ "i", "c" }
-		),
 	},
 	snippet = {
 		expand = function(args)
