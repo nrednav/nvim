b5cb4305010fe70c5a4781c703227bf8763fcf5e
Vandern Rodrigues <nrednav@gmail.com>
2025-08-31 23:55:25 +0100

    Fix treesitter config

--- lua/plugins/custom/treesitter.lua
+++ lua/plugins/custom/treesitter.lua
@@ -15,6 +15,12 @@ M.setup = function()
 		"heex",
 	}
 
+	local parsers_set = {}
+
+	for _, parser_name in ipairs(parsers_to_install) do
+		parsers_set[parser_name] = true
+	end
+
 	require("nvim-treesitter").install(parsers_to_install):wait()
 
 	local group = vim.api.nvim_create_augroup("custom-treesitter", { clear = true })
@@ -22,9 +28,15 @@ M.setup = function()
 	vim.api.nvim_create_autocmd("FileType", {
 		group = group,
 		callback = function(args)
-			local ok, parser = pcall(vim.treesitter.get_parser, args.buf)
-			if ok and parser then
-				vim.treesitter.start(args.buf)
+			local filetype = vim.bo[args.buf].filetype
+
+			if parsers_set[filetype] then
+				local ok, parser = pcall(vim.treesitter.get_parser, args.buf)
+
+				if ok and parser then
+					vim.treesitter.start(args.buf)
+					vim.bo[args.buf].syntax = "on"
+				end
 			end
 		end,
 	})
