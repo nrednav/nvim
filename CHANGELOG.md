51467ada1cf87da52829458c88266622ae626026
Vandern Rodrigues <nrednav@gmail.com>
2026-01-26 01:35:07 +0000

    Add TypeScript tooling and notification system
    This introduces three new plugins to enhance the development workflow:
    
    - `nvim-notify`: Replaces the default `vim.notify` with a visually
      appealing notification system.
    - `tsc.nvim`: Adds support for project-wide TypeScript type checking to
      catch errors asynchronously.
    - `typescript-tools.nvim`: Provides advanced TypeScript LSP features,
      configured here to enable inlay hints and automatic JSX tag closing.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,16 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic
 Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.6.0] - 2026-01-26
+
+### Added
+
+- `nvim-notify` plugin to handle system notifications, replacing the default
+  `vim.notify`.
+- `tsc.nvim` plugin for asynchronous, project-wide TypeScript type checking.
+- `typescript-tools.nvim` for TypeScript LSP integration, enabling inlay hints
+  and JSX tag closing.
+
 ## [1.5.0] - 2026-01-25
 
 ### Added
---
+++ lua/plugins/typescript-tools.lua
@@ -0,0 +1,34 @@
+return {
+  "pmizio/typescript-tools.nvim",
+  dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+  opts = {
+    separate_diagnostic_server = true,
+    publish_diagnostic_on = "insert_leave",
+    jsx_close_tag = {
+      enable = true,
+      filetypes = { "javascriptreact", "typescriptreact" }
+    },
+    expose_as_code_action = {},
+    tsserver_path = nil,
+    tsserver_plugins = {},
+    tsserver_format_options = {},
+    tsserver_file_preferences = {
+      includeInlayParameterNameHints = "all",
+      includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+      includeInlayVariableTypeHints = true,
+      includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+      includeInlayPropertyDeclarationTypeHints = true,
+      includeInlayFunctionParameterTypeHints = true,
+      includeInlayEnumMemberValueHints = true,
+      includeInlayFunctionLikeReturnTypeHints = true,
+      includeCompletionsForModuleExports = true,
+      includeCompletionsForImportStatements = true,
+    },
+    tsserver_locale = "en",
+    complete_function_calls = false,
+    include_completions_with_insert_text = true,
+    code_lens = "off",
+    disable_member_code_lens = true,
+    tsserver_max_memory = 12288
+  },
+}
